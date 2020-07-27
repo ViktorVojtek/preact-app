0105cf47aa91efbb18d71e15fa933a86381f5b9c
ViktorVojtek <viktor1vojtek@gmail.com>
2020-07-27 12:03:40 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React, { render } from 'react';
+import App from './App.js';
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    'alias': {
+      'react': 'preact/compat',
+      'react-dom': 'preact/compat'
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      h: ['preact', 'h'],
+    })
+  ]
+};
